e3c1a018647e5bfc64615952a5d01630b137b7d4
beistroff <galiasandrii@gmail.com>
2023-03-26 14:50:11 +0300

    Task 1 is done

--- OOPLab8T/Task1.cpp
+++ OOPLab8T/Task1.cpp
@@ -17,7 +17,20 @@ namespace task1_remove_min {
 
     template <>
     void remove_min<char*>(vector<char*>& charVec) {
-        
+        if (charVec.empty()) {
+            return;
+        }
+
+        const auto theSmallestWord = *min_element(charVec.begin(), charVec.end(),
+            [](const char* firstElem, const char* lastElem) {
+                return strlen(firstElem) < strlen(lastElem);
+            });
+
+        const int minLength = strlen(theSmallestWord);
+        charVec.erase(remove_if(charVec.begin(), charVec.end(), 
+            [minLength](const char* word) {
+                return strlen(word) == minLength;
+            }), charVec.end());
     }
 
     int tmain() {
@@ -50,6 +63,43 @@ namespace task1_remove_min {
         cout << "\n After removing min elems: " << endl;    // After removing
         for (const auto& elem : arr) {
             cout << elem << " ";
+        }
+
+        cout << " \n\n char template: ";
+        do {
+            cout << " Enter size of an array (1 <= n <= 20): ";
+            cin >> n;
+
+            if (cin.fail() || n < 0 || n > 20) {
+                cin.clear();
+                cout << " Invalid input." << endl;
+            }
+        } while (cin.fail() || n < 0 || n > 20);
+
+        cin.ignore();
+
+        vector<char*> words;
+        for (int i = 0; i < n; i++) {
+            char* word = new char[50];
+            cout << " Enter [" << i << "] word: ";
+            cin.getline(word, 50);
+            words.push_back(word);
+        }
+
+        cout << "\n Before removing the smallest word: " << std::endl;
+        for (auto word : words) {
+            cout << word << " ";
+        }
+
+        remove_min(words);
+        cout << "\n After removing the smallest word: " << std::endl;
+        for (auto word : words) {
+            cout << word << " ";
+        }
+        cout << "\n";
+
+        for (auto word : words) {   // free words array
+            delete[] word;
         }
 
         return 0;
